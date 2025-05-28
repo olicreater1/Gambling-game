21d57add3ee0729e063813a73418d8b2249cf17a
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2025-05-28 12:51:14 +0000

    bug fixes and ui changes

--- gambling_addiction.py
+++ gambling_addiction.py
@@ -4,7 +4,9 @@ from time import sleep
 #print ascii number boxes
 def print_number(number1, number2, number3, winning_array):
     sleep_var = 0.01
-    print(f"Winning numbers are: {round(number1)}, {round(number2)}, {round(number3)}!")
+    print("               --- --- ---")
+    print(f"Winning slots: |{round(number1)}| |{round(number2)}| |{round(number3)}|")
+    print("               --- --- ---")
     sleep(5)
     for i in range(40):
         number_1 = round(random.randint(0, 9))
@@ -17,10 +19,10 @@ def print_number(number1, number2, number3, winning_array):
         sleep_var = sleep_var +0.01
     
     print("--- --- ---")
-    print(f"|{round(number1)}| |{round(number2)}| |{round(number3)}|")
+    print(f"|{round(number_1)}| |{round(number_2)}| |{round(number_3)}|")
     print("--- --- ---")
 
-    if round(number1) == winning_array[0] and round(number2) == winning_array[1] and round(number3) == winning_array[2]:
+    if round(number_1) == round(winning_array[0]) and round(number_2) == round(winning_array[1]) and round(number_3) == round(winning_array[2]):
         return True
     else:
         return False
@@ -31,8 +33,10 @@ def gamble():
     
     if win_lose:
         print("YOU WON!!!")
+        print(" ")
     else:
-        print("You lost it all")  
+        print("You lost it all")
+        print(" ")
 def main():
     while True:
         start = input("Type 'spin' to spin: ")
